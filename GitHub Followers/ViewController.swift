5e8ae192b4d7f75f4559befe6bb0583ea43e9430
Pritesh Desai <iampriteshdesai@gmail.com>
2018-05-01 04:27:45 -0500

    Initial Commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Pritesh Desai on 5/1/18.
+//  Copyright © 2018 Little Maxima LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
